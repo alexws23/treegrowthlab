430840c1ce728c225cf64ee57bcb3044e1051d4c
alexws23 <alexws23@verizon.net>
2025-02-18 12:15:46 -0700

    push

--- codespace.R
+++ codespace.R
@@ -1,4 +1,14 @@
 library(tidyverse)
 library(usethis)
+
+# %>% cmd+sht+M
+#In Quarto, ctrl+alt+i creates code block
+
+#Connect to Git (Must be in Console)
+usethis::use_git()
+#Create Repo (Should Redirect)
+usethis::use_github()
+
+# Read CSV
 Tree <- read_csv("data/tree_dat.csv")
 glimpse(Tree)
